25ce8b211c968286b46ea562c3550e67a259a8c7
Jonathan Zempel <jzempel@gmail.com>
2012-08-31 12:27:21 -0700

    Add homebrew formula

---
+++ formula.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Continuity < Formula
+  homepage 'https://github.com/jzempel/continuity'
+  head 'https://github.com/jzempel/continuity.git'
+
+  # The pyinstaller-built binary complains on strip.
+  skip_clean 'bin'
+
+  def install
+    system "make", "prefix=#{prefix}", "install"
+  end
+
+  def test
+    system "#{bin}/continuity", "--version"
+  end
+end
